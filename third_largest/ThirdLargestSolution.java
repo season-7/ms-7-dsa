62978f5f879c3a0b59845d09d3dbcc3c6e6ccd3b
markmwaura <markkmwaura@gmail.com>
2016-07-23 23:45:50 +0300

    add new thridlargest solution

--- third_largest/ThirdLargestSolution.java
+++ third_largest/ThirdLargestSolution.java
@@ -4,7 +4,7 @@
 // your output should be "world" because "before" is 6 letters long, and "hello" and "world" are both 5, but the output should be world because it appeared as the last 5 letter word in the array. If strArr was ["hello", "world", "after", "all"] the output should be after because the first three words are all 5 letters long, so return the last one. The array will have at least three strings and each string will only contain letters.
 
 // use the main method only for calling the function where your logic lies. :-)
-public class third_largest {
+public class ThirdLargestSolution {
     public static String ThirdGreatest(String[] strArr) {
         String result = "";
         String sortedstring = "";
